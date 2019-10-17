95064f9d60a050b9c15407594a59e0f622c5b44e
mohamed.bennis <mbennis@octo.com>
2019-10-17 15:45:38 +0200

    [kodein] set up project for kodein

--- androidApp/src/main/java/com/octo/project/converter/MainActivity.kt
+++ androidApp/src/main/java/com/octo/project/converter/MainActivity.kt
@@ -3,28 +3,15 @@ package com.octo.project.converter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.octo.project.Displayer
-import com.octo.project.KotlinDi
-import com.octo.project.Presenter
 import com.octo.project.R
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), Displayer {
-
     override fun display(string: String) {
-        textView.text = string
-    }
-
-    private val kotlinDi: KotlinDi by lazy {
-        KotlinDi(this)
-    }
-
-    private val presenter: Presenter by lazy {
-        kotlinDi.getPresenter()
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        presenter.presentConcat()
     }
 }
