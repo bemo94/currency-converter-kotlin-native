be0232b0df38fa2edcc774530a4bad532dfab96b
mohamed.bennis <mbennis@octo.com>
2019-10-17 16:36:41 +0200

    [kodein] remove .idea files

--- iosApp/iosApp/ViewController.swift
+++ iosApp/iosApp/ViewController.swift
@@ -2,18 +2,20 @@
 //  ViewController.swift
 //  iosApp
 //
-//  Created by BENNIS MOHAMED on 09/05/2019.
-//  Copyright © 2019 Octo. All rights reserved.
+//  Created by mohamed bennis on 17/10/2019.
+//  Copyright © 2019 mohamed bennis. All rights reserved.
 //
 
 import UIKit
 import SharedCode
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         label.text = DummyStringKt.getDummyString()
     }
 }
+
