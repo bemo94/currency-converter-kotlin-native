86a9a9de18e468a39c892e91ca9e03a485dda22a
mohamed.bennis <mbennis@octo.com>
2019-10-17 16:11:26 +0200

    [kodein] remove .idea files

--- androidApp/src/main/java/com/octo/project/converter/MainActivity.kt
+++ androidApp/src/main/java/com/octo/project/converter/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.octo.project.converter
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.octo.project.Displayer
 import com.octo.project.R
 
